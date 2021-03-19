0d0103c4c514db4fbf9b4b3be14b1d217b244301
Karan Jagdale <37465796+KaranJagdale@users.noreply.github.com>
2021-03-19 20:36:21 +0530

    Added Comments and removed unwanted part

--- MahonyImplement_Animation_allAng_refine.py
+++ MahonyImplement_Animation_allAng_refine.py
@@ -15,23 +15,29 @@ datarate = 20 #rate of data collection in Hz
 alfam = 0.995  #parameter for low pass filter for magnetic field readings
 alfag = 0.995
 ndim = 3       #dimention of space
+
+#Function to calculate cross product matrix
 def Vectocrossm(v):
     vx = np.array([[0, -v[2], v[1]],[v[2], 0, -v[0]],[-v[1], v[0], 0]])
     return vx
 
+#Filter dynamics
 def Rcapdot(Rcap, Omegam,bcap,correction):   #arguments should be numpy arrays
     global Kp
     return Vectocrossm(Omegam - bcap) + Kp*Vectocrossm(correction)
-    
+
+#Bias dynamics
 def bcapdot(correction):
     global Ki
     return -Ki*correction
-    
+
+#Calculating wmes
 def correction(vg,vgcap,vm,vmcap):
     global Ka, Km
     return Ka*np.cross(vg,vgcap) + Km*np.cross(vm,vmcap)    
     
 #taken from https://www.learnopencv.com/rotation-matrix-to-euler-angles/ to see how the answers changes if transpose of rotation matrix is used
+#Below function is used to get Euler angles from R
 def rotationMatrixToEulerAngles(R) :
     
     sy = math.sqrt(R[0,0] * R[0,0] +  R[0,1] * R[0,1])
@@ -52,6 +58,7 @@ def rotationMatrixToEulerAngles(R) :
 
     return np.array([x, y, z])
 
+#Following function is not used for Euler angles the one which is above this function is used
 def rotationMatrixToEulerAnglesT(R) :
     
     sy = math.sqrt(R[0,0] * R[0,0] +  R[1,0] * R[1,0])
@@ -72,9 +79,8 @@ def rotationMatrixToEulerAnglesT(R) :
 
 Rcap = np.matrix([[1, 0, 0],[0, 1, 0],[0, 0, 1]])
 bcap = np.array([0.1, 0.1, 0.1])
-vlinear = np.array([0, 0, 0])    #calculate linear acceleration if required
+vlinear = np.array([0, 0, 0])    #calculate linear acceleration if required. In current code linear acceleration is not calculated
 vg0 = np.array([[0], [0], [1]])
-vm0 = np.array([[0], [1], [0]])
 vm0 = np.array([[0], [0.7071], [0.7071]])
 
 # Parameters
@@ -143,70 +149,65 @@ def calculateAng():
         if i == 0:
             nowtime = reading[0]
             
-            #print(reading[4:7])
+            #Measured angular velocity
             Omegam = reading[4:7]*pi/180
             
+            #Measured gravity vector
             vgr = reading[1:4]
             vg = vgr/(np.linalg.norm(vgr))
 
-            #vg = np.transpose(vg)
+            #Measured magnetic field vector
             vmr = reading[7:]
             vm = vmr/(np.linalg.norm(vmr))
             #vm = vmr                             #for i = 0 low pass filter reading will be the actual reading
             vgcap = np.transpose(Rcap)*vg0
             vgcap = np.array([vgcap[0,0],vgcap[1,0],vgcap[2,0]])
 
-            #Euler angles 
+            #Euler angles calculation 
             eulang = rotationMatrixToEulerAngles(Rcap)
-            
-            
             
             vmcap = np.transpose(Rcap)*vm0
             vmcap = np.array([vmcap[0,0],vmcap[1,0],vmcap[2,0]])
-            #print(vmcap)
+            #wmes calculation
             wmes = correction(vg, vgcap, vm, vmcap)
-            #Wmes[i,:] = wmes                      #storing the correction values
+            #Calculating filter dynamics                     
             dotR = Rcapdot(Rcap, Omegam, bcap, wmes)
             
             prevtime = nowtime
-            
-           
-            
-            
+        
             
         if i != 0:   
             nowtime = reading[0]
             delt = nowtime - prevtime
             print('delt : ', delt)
+            
+            #Updating rotation matrix estimate
             Rcap = Rcap*sc.expm(dotR*delt)
-#            if nowtime > 10 and nowtime < 20:
-#                print(Rcap)
-                
+
+            #Measured angular velocity    
             Omegam = reading[4:7]*pi/180  
             
+            #Measured gravity vector
             vgr = reading[1:4]
-            
-           
             vg = vgr/(np.linalg.norm(vgr))
             
-            vmr = reading[7:]
-            
-            
+            #Measured magnetic field vector
+            vmr = reading[7:] 
             vm = vmr/(np.linalg.norm(vmr))
             
             vgcap = np.transpose(Rcap)*vg0
             vgcap = np.array([vgcap[0,0],vgcap[1,0],vgcap[2,0]])
             
         
-            #Euler angles of transpse of matrix
+            #Euler angles calculation
             eulang = rotationMatrixToEulerAngles(Rcap)
             
-    
             vmcap = np.transpose(Rcap)*vm0
             vmcap = np.array([vmcap[0,0],vmcap[1,0],vmcap[2,0]])
-        #print(vmcap)
+            #Calculating wmes
             wmes = correction(vg, vgcap, vm, vmcap)
-            #Wmes[i,:] = wmes 
+            
+            #FIlter dynamics calculation
             dotR = Rcapdot(Rcap, Omegam, bcap, wmes)
             
             prevtime = nowtime
@@ -219,8 +220,6 @@ def calculateAng():
 
 # This function is called periodically from FuncAnimation
 def animate1(j, ys1):
-
-    # Read temperature (Celsius) from TMP102
     
     angle = calculateAng() 
     #temp_c = np.random.random()
@@ -239,8 +238,6 @@ def animate1(j, ys1):
     return line1,
     
 def animate2(j, ys2):
-
-    # Read temperature (Celsius) from TMP102
     
     angle = calculateAng() 
     #temp_c = np.random.random()
@@ -260,8 +257,6 @@ def animate2(j, ys2):
     return line2,
     
 def animate3(j, ys3):
-
-    # Read temperature (Celsius) from TMP102
     
     angle = calculateAng() 
     #temp_c = np.random.random()
